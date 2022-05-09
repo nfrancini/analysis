103885de65d77845a89a80449d779596ae22521a
n.francini <nicco.francini@libero.it>
2022-05-09 17:32:48 +0200

    Backup

--- FO_analysis/linear_fit.py
+++ FO_analysis/linear_fit.py
@@ -15,10 +15,10 @@ def linear_2(x, c, d):
     return 0.25*c*x + d
 
 # CARICO I DATI (CHE SIANO C OD U CAMBIO SOLO IL LINK)
-L, y1, dy1, y2, dy2 = np.genfromtxt('./C_J_N2_K0.4.dat', delimiter = '\t', unpack = True)
+L, y1, dy1, y2, dy2 = np.genfromtxt('./prova_gauge.dat', delimiter = '\t', unpack = True)
 
 # EVENTUALE TAGLIO DEI DATI
-l_min = 10
+l_min = 9
 l_max = 65
 
 mask = ((L>=l_min) & (L<=l_max))
@@ -59,52 +59,52 @@ pl.legend()
 # pl.savefig('../grafici/continuo/K_0.4/jc_vs_V.png')
 
 # ANALISI PER L'ANDAMENTO DI C_MAX OD U
-# popt, pcov = curve_fit(linear_2, 1.0/x_masked, y2_masked, sigma = dy2_masked, absolute_sigma = True)
-#
-# # STAMPO I RISULTATI
-# chiq = np.sum( ((y2_masked - linear_2(1.0/x_masked, *popt)) / (dy2_masked))**2 )
-# ndof = len(1.0/x_masked) - len(popt)
-# red_chiq = chiq/ndof
-# perr = np.sqrt(np.diag(pcov))
-# print('delta_h^2 = %f +- %f' % (popt[0], perr[0]))
-# print('m= %f +- %f' % (popt[1], perr[1]))
-# print('red_chiq = %f' % (red_chiq))
-#
-# # PLOT
-# pl.figure(2)
-# xx = np.linspace(np.min(1.0/x_masked), np.max(1.0/x_masked), 500)
-# y = linear_2(xx, *popt)
-# pl.errorbar(1.0/x, y2, dy2, ls = '', marker = 'o', fillstyle = 'none')
-# pl.plot(xx, y)
-
-L, y, dy = np.genfromtxt('./intersection_N2_K0.4.dat', delimiter = '\t', unpack = True)
-
-x = 1/L**3
-
-popt, pcov = curve_fit(linear_1, x, y, sigma = dy, absolute_sigma = True)
+popt, pcov = curve_fit(linear_2, 1.0/x_masked, y2_masked, sigma = dy2_masked, absolute_sigma = True)
 
 # STAMPO I RISULTATI
-chiq = np.sum( ((y - linear_1(x, *popt)) / (dy))**2 )
-ndof = len(x) - len(popt)
+chiq = np.sum( ((y2_masked - linear_2(1.0/x_masked, *popt)) / (dy2_masked))**2 )
+ndof = len(1.0/x_masked) - len(popt)
 red_chiq = chiq/ndof
 perr = np.sqrt(np.diag(pcov))
-print('jc = %f +- %f' % (popt[0], perr[0]))
-print('m = %f +- %f' % (popt[1], perr[1]))
+print('delta_h^2 = %f +- %f' % (popt[0], perr[0]))
+print('m= %f +- %f' % (popt[1], perr[1]))
 print('red_chiq = %f' % (red_chiq))
 
 # PLOT
-c_style, m_style = set_style()
 pl.figure(2)
-c = next(c_style)
-m = next(m_style)
+xx = np.linspace(np.min(1.0/x_masked), np.max(1.0/x_masked), 500)
+y = linear_2(xx, *popt)
+pl.errorbar(1.0/x, y2, dy2, ls = '', marker = 'o', fillstyle = 'none')
+pl.plot(xx, y)
 
-xx = np.linspace(np.min(x), np.max(x), 500)
-yy = linear_1(xx, *popt)
-pl.errorbar(x, y, dy, ls = '', marker = m, color = c, fillstyle = 'none', label = 'Misure')
-pl.plot(xx, yy, color = c, label = 'Fit')
-pl.xlabel(r'$V^{-1}$')
-pl.ylabel(r'$J_c^{int}$')
-pl.legend()
-# pl.savefig('../grafici/continuo/K_0.4/jc_intersect_V.png')
+# L, y, dy = np.genfromtxt('./intersection_N2_K0.4.dat', delimiter = '\t', unpack = True)
+#
+# x = 1/L**3
+#
+# popt, pcov = curve_fit(linear_1, x, y, sigma = dy, absolute_sigma = True)
+#
+# # STAMPO I RISULTATI
+# chiq = np.sum( ((y - linear_1(x, *popt)) / (dy))**2 )
+# ndof = len(x) - len(popt)
+# red_chiq = chiq/ndof
+# perr = np.sqrt(np.diag(pcov))
+# print('jc = %f +- %f' % (popt[0], perr[0]))
+# print('m = %f +- %f' % (popt[1], perr[1]))
+# print('red_chiq = %f' % (red_chiq))
+#
+# # PLOT
+# c_style, m_style = set_style()
+# pl.figure(2)
+# c = next(c_style)
+# m = next(m_style)
+#
+# xx = np.linspace(np.min(x), np.max(x), 500)
+# yy = linear_1(xx, *popt)
+# pl.errorbar(x, y, dy, ls = '', marker = m, color = c, fillstyle = 'none', label = 'Misure')
+# pl.plot(xx, yy, color = c, label = 'Fit')
+# pl.xlabel(r'$V^{-1}$')
+# pl.ylabel(r'$J_c^{prova}$')
+# pl.legend()
+# pl.savefig('../grafici/continuo/K_0.4/jc_U_intersect_V.png')
 
 pl.show()
--- FO_intersection.py
+++ FO_intersection.py
@@ -22,17 +22,15 @@ def FO_fit(x, y, dy, j_min, j_max, n_term1, n_term2):
     initParam = np.zeros(n_term1)
     for n_term in tqdm(range(n_term1, n_term2)):
         popt, pcov = curve_fit(poly, x, y, p0=initParam, sigma = dy, absolute_sigma=True)
-        # popt, stats = np.polynomial.polynomial.polyfit(x, y, n_term-1, full = True, w = 1.0/(dy*100))
-        # initParam = popt//
         initParam =  np.zeros(n_term+1)
 
         chiq = np.sum(( (y - poly(x, *popt)) / dy )**2)
         ndof = len(y) - len(initParam)
         red_chiq = chiq/ndof
-        # red_chiq = stats[0]/ndof
         red_chisq = np.append(red_chisq, red_chiq)
         term = np.append(term, n_term)
         cc.append(popt[0:])
+
 
     delta = np.abs(1-red_chisq)
     p = cc[np.argmin(delta)]
@@ -56,8 +54,14 @@ def FO_intersection(path_filenames, j_min, j_max, l_min, l_max, n_term_min, n_te
                 sorted_J = aux_J[aux_J.argsort()]
                 sorted_U = aux_U[aux_J.argsort()]
                 sorted_err_U = aux_err_U[aux_J.argsort()]
+                sorted_corr_len = aux_corr_len[aux_J.argsort()]
+                sorted_err_corr_len = aux_err_corr_len[aux_J.argsort()]
+                sorted_L = aux_L[aux_J.argsort()]
 
-                p, red_chisq_opt_FO, n_term_opt = FO_fit(sorted_J, sorted_U, sorted_err_U, j_min, j_max, n_term1, n_term2)
+                # p, red_chisq_opt_FO, n_term_opt = FO_fit(sorted_J, sorted_U, sorted_err_U, j_min, j_max, n_term1, n_term2)
+                # coeffs.append(p)
+
+                p, red_chisq_opt_FO, n_term_opt = FO_fit(sorted_J, sorted_corr_len/sorted_L, sorted_err_corr_len/sorted_L, j_min, j_max, n_term1, n_term2)
                 coeffs.append(p)
 
                 n_term1 = int(n_term_opt)
@@ -66,8 +70,9 @@ def FO_intersection(path_filenames, j_min, j_max, l_min, l_max, n_term_min, n_te
                 # FARE PLOT DI CONTROLLO
                 if(ctrl_plot == True):
                     curve = np.polynomial.Polynomial(p)
-                    x = np.linspace(j_min, j_max, 500)
-                    pl.errorbar(sorted_J, sorted_U, sorted_err_U, ls = '', fillstyle = 'none', marker = 'o', label = 'L='+str(int(aux_L[0])))
+                    x = np.linspace(sorted_J.min(), sorted_J.max(), 10000)
+                    # pl.errorbar(sorted_J, sorted_U, sorted_err_U, ls = '', fillstyle = 'none', marker = 'o', label = 'L='+str(int(aux_L[0])))
+                    pl.errorbar(sorted_J, sorted_corr_len/sorted_L, sorted_err_corr_len/sorted_L, ls = '', fillstyle = 'none', marker = 'o', label = 'L='+str(int(aux_L[0])))
                     pl.plot(x, curve(x))
                     pl.xlim(j_min, j_max)
                     pl.legend()
@@ -87,59 +92,4 @@ def FO_intersection(path_filenames, j_min, j_max, l_min, l_max, n_term_min, n_te
     mean_intersect = np.mean(intersection)
     err_intersect = (max(intersection) - min(intersection)) / 2.0
 
-    return mean_intersect, err_intersect, n_term_opt
-
-# def FO_quadratic(path_filenames, j_min, j_max, l_min, l_max, ctrl_plot):
-#     def quadratic(x, a, b, c):
-#         return a*(x-b)**2 + c
-#
-#     for fname in path_filenames:
-#         with open(fname) as infile:
-#             aux_L, aux_J, aux_K, aux_ene_sp, aux_err_ene_sp, aux_ene_g, aux_err_ene_g, aux_ene_dens, aux_err_ene_dens, aux_susc, aux_err_susc, aux_G_pm, aux_err_G_pm, aux_C, aux_err_C, aux_U, aux_err_U, aux_corr_len, aux_err_corr_len, aux_K2_g, aux_err_K2_g, aux_K2_sp, aux_err_K2_sp, aux_K3_g, aux_err_K3_g, aux_K3_sp, aux_err_K3_sp = np.genfromtxt(infile, delimiter ="\t", unpack = True)
-#             # print(aux_C, len(aux_C))
-#             # print(aux_err_C, len(aux_err_C))
-#             if((aux_L[0]>=l_min) & (aux_L[0]<=l_max)):
-#                 sorted_J = aux_J[aux_J.argsort()]
-#                 sorted_U = aux_U[aux_J.argsort()]
-#                 sorted_err_U = aux_err_U[aux_J.argsort()]
-#                 sorted_C = aux_C[aux_J.argsort()]
-#                 sorted_err_C = aux_err_C[aux_J.argsort()]
-#
-#                 mask = ((sorted_J >= j_min) & (sorted_J <= j_max))
-#                 sorted_J = sorted_J[mask]
-#                 sorted_U = sorted_U[mask]
-#                 sorted_C = sorted_C[mask]
-#                 sorted_err_U = sorted_err_U[mask]
-#                 sorted_err_C = sorted_err_C[mask]
-#
-#                 # TROVO IL MASSIMO E LO METTO NEI PARAMETRI INIZIALI
-#                 C_crit = np.max(sorted_C)
-#                 J_crit = sorted_J[np.argmax(sorted_C)]
-#                 a_init = (sorted_C[0] - C_crit) / (sorted_J[0] - J_crit)**2
-#
-#                 initParam = [a_init, J_crit, C_crit]
-#
-#                 # PRENDO SOLO I SEI PUNTI PRIMA E DOPO IL PICCO
-#                 cut_J = sorted_J[np.argmax(sorted_C)-1 : np.argmax(sorted_C) + 2]
-#                 cut_C = sorted_C[np.argmax(sorted_C)-1 : np.argmax(sorted_C) + 2]
-#                 cut_err_C = sorted_err_C[np.argmax(sorted_C)-1 : np.argmax(sorted_C) + 2]
-#
-#                 # PROVO IL FIT QUADRATICO
-#                 popt, pcov = curve_fit(quadratic, cut_J, cut_C, p0 = initParam, sigma = cut_err_C, absolute_sigma = True)
-#                 chiq = np.sum(( (cut_C - quadratic(cut_J, *popt)) / cut_err_C )**2)
-#                 ndof = len(cut_C) - len(initParam)
-#                 perr = np.sqrt(np.diag(pcov))
-#                 red_chiq = chiq/ndof
-#                 print(red_chiq)
-#                 print("a = %f +- %f" % (popt[0], perr[0]))
-#                 print("b = %f +- %f" % (popt[1], perr[1]))
-#                 print("c = %f +- %f" % (popt[2], perr[2]))
-#
-#                 # FARE PLOT DI CONTROLLO
-#                 if(ctrl_plot == True):
-#                     x = np.linspace(np.min(cut_J), np.max(cut_J), 500)
-#                     y = quadratic(x, *popt)
-#                     pl.errorbar(cut_J, cut_C, cut_err_C, ls = '', fillstyle = 'none', marker = 'o', label = 'L='+str(int(aux_L[0])))
-#                     pl.plot(x,y)
-#                     pl.legend()
-#     return popt
+    return mean_intersect, err_intersect, n_term_opt, red_chisq_opt_FO
